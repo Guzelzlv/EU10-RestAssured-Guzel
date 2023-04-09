e6877b28522aeafb4a8434dca81169483323ea85
Guzel <kz_guzel@yahoo.com>
2023-04-09 05:09:26 +0100

    day4 day5

--- src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
@@ -52,7 +52,14 @@ public class ORDSApiWithJsonPath extends HRTestBase {
         System.out.println(employeeItProgs);
 
         //get me first name of employees who is making more than 10000
-        List<String> emplNames = jsonPath.getList("items.findAll{it.salary>10000}.first_name");
-        System.out.println(emplNames);
+        List<String> empNames = jsonPath.getList("items.findAll{it.salary>10000}.first_name");
+        System.out.println(empNames);
+
+        //get the max salary first_name
+        String kingFirstName = jsonPath.getString("items.max {it.salary}.first_name");
+        String kingNameWithPathMethod = response.path("items.max {it.salary}.first_name");
+        System.out.println("kingFirstName = " + kingFirstName);
+        System.out.println("kingNameWithPathMethod = " + kingNameWithPathMethod);
+
     }
 }
