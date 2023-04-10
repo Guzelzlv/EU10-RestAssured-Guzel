524c91101c4e7fd4f8e8dda98ba9d9fbeb412497
Guzel <kz_guzel@yahoo.com>
2023-04-10 02:40:36 +0100

    commit

--- src/test/java/com/cydeo/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cydeo/day5/HamcrestMatchersApiTest.java
@@ -1,12 +1,12 @@
 package com.cydeo.day5;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
-
 
 
 public class HamcrestMatchersApiTest {
@@ -28,8 +28,56 @@ public class HamcrestMatchersApiTest {
     @Test
     public void test1() {
 
-
+        given()
+                 .accept(ContentType.JSON)
+                    .and().pathParam("id",15)
+                .when()
+                    .get("http://3.86.198.182:8000/api/spartans/{id}")
+                .then()
+                    .statusCode(200)
+                    .and()
+                    .contentType("application/json")
+                    .body("id",equalTo(15),
+                    "name",is("Meta"),
+                            "gender",is("Female"),
+                            "phone", is(1938695106));
 
     }
 
+    @DisplayName("CBTraining Teacher request with chaining and matchers")
+    @Test
+    public void teacherData(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("id",3)
+                .when()
+                .get("https://api.training.cydeo.com/teacher/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                .and()
+                .header("Date",notNullValue())
+                .and().assertThat()
+                .body("teachers[0].firstName",is("Tet"))
+                .body("teachers[0].lastName",is("DS"))
+                .body("teachers[0].gender",equalTo("Male"));
     }
+
+    @DisplayName("GET request to teacher/all and chaining")
+    @Test
+    public void teachersTest() {
+
+        //verify Alexander,Darleen,Sean inside the all teachers
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://api.training.cydeo.com/teacher/all")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("teachers.firstName",hasItems("Valter","Mario","Porter"));
+    }
+}
--- src/test/java/com/cydeo/utilities/HRTestBase.java
+++ src/test/java/com/cydeo/utilities/HRTestBase.java
@@ -1,5 +1,6 @@
 package com.cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -10,6 +11,18 @@ public abstract class HRTestBase {
     //save baseurl inside this variable so that we dont need to type each http method.
     public static void init() {
         baseURI = "http://3.86.198.182:1000/ords/hr";
-    }
 
+    //get ip address from configuraitons
+    String dbUrl = "jdbc:oracle:thin:@3.86.198.182:1521:xe";
+    String dbUsername = "SP";
+    String dbPassword = "SP";
+
+    //  DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+}
+
+    @AfterAll
+    public static void teardown(){
+
+        //DBUtils.destroy();
+    }
 }
